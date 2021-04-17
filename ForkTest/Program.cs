9051cee0f0951f736b241f3e968869f5d7e89e10
Belekz <pieter.dewitte@hotmail.be>
2021-04-17 17:47:47 +0200

    changed for new user

--- ForkTest/Program.cs
+++ ForkTest/Program.cs
@@ -6,7 +6,7 @@ namespace ForkTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello test user!");
+            Console.WriteLine("Hello new user!");
         }
     }
 }
