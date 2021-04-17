3e868c45126ea3c105ebaee7fed6008f4c319cf9
Belekz <pieter.dewitte@hotmail.be>
2021-04-17 17:31:26 +0200

    Add project files.

---
+++ ForkTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ForkTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
